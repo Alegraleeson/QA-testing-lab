72c21f7daf1c258976193c40b737bbef723f4f8e
Alegraleeson <wingmonkee@yahoo.com>
2022-07-25 16:41:11 -0500

    first commit

---
+++ functions.test.js
@@ -0,0 +1,27 @@
+const {
+    greeting, 
+    add 
+    } = require('./functions')
+
+test("should run", () => expect(2).toBe(2));
+
+describe("greeting tests", () => {
+    test("greeting should equal hello james", () => {
+        expect(greeting('James')).toEqual('Hello, James.')
+    });
+    test("greeting should equal hello Jill", () => {
+        expect(greeting('Jill')).toEqual('Hello, Jill.')
+    })
+});
+
+describe("add tests", () => {
+    test('add should equal 3', () => {
+        const result = add(1,2);
+      expect(result).toEqual(3);
+    });
+    test('add should equal 14', () => {
+        const result = add(5,9);
+        expect(result).toEqual(14);
+    });
+
+})
